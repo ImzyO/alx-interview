82d9cf661b9ba854d25a813900ca11cac8b8fcb3
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-12-22 12:57:44 +0000

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+returns a list of lists of integers
+representing the Pascal’s triangle of n:
+"""
+def pascal_triangle(n):
+    """returns list of integers"""
+    t_row = [1]
+    temp_l = [0]
+    pTri = []
+
+    if n <= 0:
+        return pTri
+
+    for i in range(n):
+        pTri.append(t_row)
+        t_row = [l+r for l, r in zip(t_row + temp_l, temp_l + t_row)]
+
+    return pTri

52f80b766c82861dfce7a85256569e602b75d9c5
Imani <100032916+ImzyO@users.noreply.github.com>
2023-01-24 15:55:25 +0300

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
+"""UTF-8 validator"""
 
-""" 5. LFU Caching
-"""
 
-from enum import Enum
-from heapq import heappush, heappop
-from itertools import count
+def validUTF8(data):
+    """
+        Check that a sequence of byte values follows the UTF-8 encoding
+        rules.  Does not check for canonicalization (i.e. overlong encodings
+        are acceptable).
+        """
 
-BaseCaching = __import__("base_caching").BaseCaching
+    data = iter(data)
+    for leading_byte in data:
+        leading_ones = _count_leading_ones(leading_byte)
+        if leading_ones in [1, 7, 8]:
+            return False
+        for _ in range(leading_ones - 1):
+            trailing_byte = next(data, None)
+            if trailing_byte is None or trailing_byte >> 6 != 0b10:
+                return False
+    return True
 
-class HeapItemStatus(Enum):
+
+def _count_leading_ones(byte):
+    """Counts the leading ones."""
+
+    for i in range(8):
+        if byte >> 7 - i == 0b11111111 >> 7 - i & ~1:
+            return i
+    return 8
